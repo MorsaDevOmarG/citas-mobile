26b8b1fcaae8e381fbc419b5442770cab462c187
Omar <omar.garcia@morsa.com.mx>
2025-12-01 17:44:36 -0600

    JSX

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, SafeAreaView} from 'react-native';
+import { Text, SafeAreaView, View} from 'react-native';
 
 const App = () => {
 
   return (
     <SafeAreaView>
-      <Text>Hola Mundo</Text>
+      <Text>
+        Administrador de Citas {''}
+          <Text>Veterinaria</Text>
+      </Text>
     </SafeAreaView>
   );
 };
