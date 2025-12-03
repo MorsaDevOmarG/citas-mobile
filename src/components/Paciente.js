3d4222a7ac352d40426efbc3110e48f118d5d965
Omar <omar.garcia@morsa.com.mx>
2025-12-03 14:05:42 -0600

    Creando BOTONES para EDITAR y ELIMINAR

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 const Paciente = ({ item }) => {
   // console.log(item);
@@ -18,6 +18,16 @@ const Paciente = ({ item }) => {
       <Text style={styles.label}>Paciente:</Text>
       <Text style={styles.texto}>{paciente}</Text>
       <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
+
+      <View style={styles.contenedorBotones}>
+        <Pressable style={[styles.btnEditar, styles.btn]}>
+          <Text style={styles.btnTexto}>Editar</Text>
+        </Pressable>
+
+        <Pressable style={[styles.btnEliminar, styles.btn]}>
+          <Text style={styles.btnTexto}>Eliminar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -46,7 +56,34 @@ const styles = StyleSheet.create({
 
   fecha: {
     color: '#374151',
-  }
+  },
+
+  contenedorBotones: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+
+  btn: {
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+
+  btnEditar: {
+    backgroundColor: '#F59E0B',
+  },
+
+  btnEliminar: {
+    backgroundColor: '#EF4444',
+  },
+
+  btnTexto: {
+    color: '#FFF',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    fontSize: 12  
+  },
 });
 
 export default Paciente;
