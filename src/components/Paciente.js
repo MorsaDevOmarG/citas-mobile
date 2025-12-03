6119ae41f5acd379c9b0151cea407a2b089fc72c
Omar <omar.garcia@morsa.com.mx>
2025-12-03 13:59:54 -0600

    Estilos al COMPONENTE PACIENTE

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -14,11 +14,39 @@ const Paciente = ({ item }) => {
   };
 
   return (
-    <View>
-      <Text>{paciente}</Text>
-      <Text>{formatearFecha(fecha)}</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.label}>Paciente:</Text>
+      <Text style={styles.texto}>{paciente}</Text>
+      <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderBottomColor: '#94A3B8',
+    borderBottomWidth: 1,
+  },
+
+  label: {
+    color: '#374151',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    marginBottom: 10
+  },
+
+  texto: {
+    color: '#6D28D9',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 10
+  },
+
+  fecha: {
+    color: '#374151',
+  }
+});
 
 export default Paciente;
